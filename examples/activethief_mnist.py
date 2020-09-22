e39e6694ee7f4369a7d38c85a33d9c2df0ac456a
Shanigen <vkaraf@gmail.com>
2020-09-22 14:50:09 +0200

    Corrected mistake and formatting in examples

--- examples/activethief_mnist.py
+++ examples/activethief_mnist.py
@@ -1,12 +1,12 @@
 import argparse
-import sys, os
+import os
+import sys
 
-import numpy as np
 import torch
 import torch.nn.functional as F
 from ignite.contrib.handlers import ProgressBar
 from ignite.engine import create_supervised_trainer
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST, CIFAR10
 from torchvision.transforms import transforms
 
@@ -20,21 +20,23 @@ from mef.utils.ios import mkdir_if_missing
 
 input_dimension = (1, 28, 28)
 
+
 def parse_args():
     parser = argparse.ArgumentParser("ActiveThief - MNIST example")
-    parser.add_argument("-c","--config_file", type=str, default="./config.yaml",
+    parser.add_argument("-c", "--config_file", type=str, default="./config.yaml",
                         help="path to configuration file")
-    parser.add_argument("-d","--data_root", type=str, default="./data",
+    parser.add_argument("-d", "--data_root", type=str, default="./data",
                         help="path to folder where datasets will be downloaded")
-    parser.add_argument("-s","--save_loc", type=str, default="./cache/activethief/MNIST",
+    parser.add_argument("-s", "--save_loc", type=str, default="./cache/activethief/MNIST",
                         help="path to folder where attack's files will be saved")
-    parser.add_argument("-t","--train_epochs", type=int, default=10,
+    parser.add_argument("-t", "--train_epochs", type=int, default=10,
                         help="number of trainining epochs for the secre_model")
-    parser.add_argument("-g","--gpu", type=bool, default=False,
+    parser.add_argument("-g", "--gpu", type=bool, default=False,
                         help="whether gpu should be used")
-    
+
     args = parser.parse_args()
     return args
+
 
 def set_up(args):
     data_root = args.data_root
@@ -44,23 +46,23 @@ def set_up(args):
     device = "cuda" if args.gpu else "cpu"
 
     secret_details = ModelDetails(net=dict(name="mnet",
-                                            act="relu",
-                                            drop="none",
-                                            pool="max_2",
-                                            ks=3,
-                                            n_conv=3,
-                                            n_fc=3), )
+                                           act="relu",
+                                           drop="none",
+                                           pool="max_2",
+                                           ks=3,
+                                           n_conv=3,
+                                           n_fc=3), )
     substitute_details = ModelDetails(net=dict(name="mnet",
-                                                act="relu",
-                                                drop="none",
-                                                pool="max_2",
-                                                ks=3,
-                                                n_conv=3,
-                                                n_fc=3))
+                                               act="relu",
+                                               drop="none",
+                                               pool="max_2",
+                                               ks=3,
+                                               n_conv=3,
+                                               n_fc=3))
     secret_model = Mnet(input_dimensions=input_dimension, num_classes=10,
-                                model_details=secret_details)
+                        model_details=secret_details)
     substitute_model = Mnet(input_dimensions=input_dimension, num_classes=10,
-                                    model_details=substitute_details)
+                            model_details=substitute_details)
 
     if device == "cuda":
         secret_model.cuda()
@@ -71,19 +73,19 @@ def set_up(args):
     transform = [transforms.Resize(input_dimension[1:]), transforms.ToTensor()]
     mnist = dict()
     mnist["train"] = MNIST(root=data_root, download=True,
-                            transform=transforms.Compose(transform))
+                           transform=transforms.Compose(transform))
 
     mnist["test"] = MNIST(root=data_root, train=False, download=True,
-                            transform=transforms.Compose(transform))
+                          transform=transforms.Compose(transform))
     test_dataset = mnist["test"]
 
     transform = [transforms.Resize(input_dimension[1:]), transforms.Grayscale(),
-                    transforms.ToTensor()]
+                 transforms.ToTensor()]
     cifar10 = dict()
     cifar10["train"] = CIFAR10(root=data_root, download=True,
-                                transform=transforms.Compose(transform))
+                               transform=transforms.Compose(transform))
     cifar10["test"] = CIFAR10(root=data_root, download=True,
-                                train=False, transform=transforms.Compose(transform))
+                              train=False, transform=transforms.Compose(transform))
 
     thief_dataset = cifar10["train"]
     validation_dataset = cifar10["test"]
@@ -97,7 +99,7 @@ def set_up(args):
         # Prepare secret model
         print("Training secret model")
         train_loader = DataLoader(dataset=mnist["train"], batch_size=128, shuffle=True,
-                                    num_workers=4, pin_memory=True)
+                                  num_workers=4, pin_memory=True)
         optimizer = torch.optim.Adam(secret_model.parameters())
         loss_function = F.cross_entropy
         trainer = create_supervised_trainer(secret_model, optimizer, loss_function,
@@ -107,8 +109,10 @@ def set_up(args):
 
         torch.save(dict(state_dict=secret_model.state_dict()), secret_savel_loc)
 
-    return dict(secret_model=secret_model, substitute_model=substitute_model, test_dataset=test_dataset,
+    return dict(secret_model=secret_model, substitute_model=substitute_model,
+                test_dataset=test_dataset,
                 validation_dataset=validation_dataset, thief_dataset=thief_dataset)
+
 
 if __name__ == "__main__":
     args = parse_args()
--- examples/copycat_goc.py
+++ examples/copycat_goc.py
@@ -1,5 +1,6 @@
 import argparse
-import sys, os
+import os
+import sys
 
 import numpy as np
 import torch
@@ -18,25 +19,28 @@ from mef.models.vision.vgg import Vgg
 from mef.utils.details import ModelDetails
 from mef.utils.ios import mkdir_if_missing
 
+
 def parse_args():
     parser = argparse.ArgumentParser("ActiveThief - MNIST example")
-    parser.add_argument("-c","--config_file", type=str, default="./config.yaml",
+    parser.add_argument("-c", "--config_file", type=str, default="./config.yaml",
                         help="path to configuration file")
-    parser.add_argument("-d","--data_root", type=str, default="./data",
+    parser.add_argument("-d", "--data_root", type=str, default="./data",
                         help="path to folder where datasets will be downloaded")
-    parser.add_argument("-i","--imagenet_root", type=str,
+    parser.add_argument("-i", "--imagenet_root", type=str,
                         help="path to folder where ImageNet2012 is located")
-    parser.add_argument("-s","--save_loc", type=str, default="./cache/copycat/GOC",
+    parser.add_argument("-s", "--save_loc", type=str, default="./cache/copycat/GOC",
                         help="path to folder where attack's files will be saved")
-    parser.add_argument("-n","--npd_size", type=int, default=2000,
-                        help="size of the non problem domain dataset that should be taken from ImageNet2012")
-    parser.add_argument("-t","--train_epochs", type=int, default=10,
+    parser.add_argument("-n", "--npd_size", type=int, default=2000,
+                        help="size of the non problem domain dataset that should be taken from "
+                             "ImageNet2012")
+    parser.add_argument("-t", "--train_epochs", type=int, default=10,
                         help="number of trainining epochs for the target model and pd_ol model")
-    parser.add_argument("-g","--gpu", type=bool, default=False,
+    parser.add_argument("-g", "--gpu", type=bool, default=False,
                         help="whether gpu should be used")
-    
+
     args = parser.parse_args()
     return args
+
 
 def remove_class(class_to_remove, labels, data, class_to_idx, classes):
     class_idx = class_to_idx[class_to_remove]
@@ -58,7 +62,7 @@ def remove_class(class_to_remove, labels, data, class_to_idx, classes):
     return labels.tolist(), data, class_to_idx, classes
 
 
-def train_model(model, train_data, save_loc):
+def train_model(model, train_data, save_loc, train_epochs, device):
     train_loader = DataLoader(dataset=train_data, batch_size=128,
                               shuffle=True, num_workers=1, pin_memory=True)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
@@ -73,6 +77,7 @@ def train_model(model, train_data, save_loc):
 
     return
 
+
 def set_up(args):
     data_root = args.data_root
     imagenet_root = args.imagenet_root
@@ -84,18 +89,18 @@ def set_up(args):
     device = "cuda" if args.gpu else "cpu"
 
     model_details = ModelDetails(net=dict(name="vgg_16",
-                                            act="relu",
-                                            drop="none",
-                                            pool="max_2",
-                                            ks=3,
-                                            n_conv=13,
-                                            n_fc=3))
+                                          act="relu",
+                                          drop="none",
+                                          pool="max_2",
+                                          ks=3,
+                                          n_conv=13,
+                                          n_fc=3))
     target_model = Vgg(input_dimensions=(3, 64, 64), num_classes=9,
-                            model_details=model_details)
+                       model_details=model_details)
     opd_model = Vgg(input_dimensions=(3, 64, 64), num_classes=9,
-                            model_details=model_details)
+                    model_details=model_details)
     copycat_model = Vgg(input_dimensions=(3, 64, 64), num_classes=9,
-                                model_details=model_details)
+                        model_details=model_details)
 
     if device == "cuda":
         target_model.cuda()
@@ -106,23 +111,23 @@ def set_up(args):
     mean = (0.5, 0.5, 0.5)
     std = (0.5, 0.5, 0.5)
     transform = [transforms.Resize((64, 64)), transforms.ToTensor(), transforms.Normalize(mean,
-                                                                                            std)]
+                                                                                          std)]
 
     cifar10 = dict()
     cifar10["train"] = CIFAR10(root=data_root, download=True,
-                                transform=transforms.Compose(transform))
+                               transform=transforms.Compose(transform))
 
     cifar10["test"] = CIFAR10(root=data_root, train=False, download=True,
-                                transform=transforms.Compose(transform))
+                              transform=transforms.Compose(transform))
 
     transform = [transforms.Resize((64, 64)), transforms.ToTensor(), transforms.Normalize(mean,
-                                                                                            std)]
+                                                                                          std)]
     stl10 = dict()
     stl10["train"] = STL10(root=data_root, transform=transforms.Compose(transform),
-                            download=True)
+                           download=True)
     stl10["test"] = STL10(root=data_root, split="test",
-                            transform=transforms.Compose(transform),
-                            download=True)
+                          transform=transforms.Compose(transform),
+                          download=True)
 
     # Replace car with automobile to make the class name same as in the cifar10
     for setx in stl10.values():
@@ -169,7 +174,7 @@ def set_up(args):
         print("Loaded target model")
     except FileNotFoundError:
         print("Training target model")
-        train_model(target_model, od_dataset, target_save_loc)
+        train_model(target_model, od_dataset, target_save_loc, train_epochs, device)
 
     # Prepare PD-OL model
     try:
@@ -178,7 +183,7 @@ def set_up(args):
         print("Loaded PD-OL model")
     except FileNotFoundError:
         print("Training PD-OL model")
-        train_model(opd_model, pd_dataset, target_save_loc)
+        train_model(opd_model, pd_dataset, target_save_loc, train_epochs, device)
 
     return dict(target_model=target_model, opd_model=opd_model, copycat_model=copycat_model,
                 test_dataset=test_dataset, pd_dataset=pd_dataset, npd_dataset=npd_dataset)
