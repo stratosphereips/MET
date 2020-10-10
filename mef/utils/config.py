61f4de7cd3f4361f8ead8315af689a7695ccc3d1
Shanigen <vkaraf@gmail.com>
2020-10-10 14:30:38 +0200

    First step of removing config.yaml requirement

--- mef/utils/config.py
+++ mef/utils/config.py
@@ -1,31 +1,40 @@
-import yaml
+# import yaml
 from dacite import from_dict
 
 
-class Configuration:
-    config_file = None
-    configs = None
+#
+#
+# class Configuration:
+#     config_file = None
+#     configs = None
+#
+#     def __init__(self, config_file):
+#         Configuration.config_file = config_file
+#         try:
+#             with open(self.config_file) as f:
+#                 Configuration.configs = yaml.full_load(f)
+#         except FileNotFoundError:
+#             print("Configuration file {} was not found.".format(
+#                 self.config_file))
+#
+#     @staticmethod
+#     def _find_config(configs, keys):
+#         for key in configs.keys():
+#             if key == keys[0]:
+#                 if isinstance(configs[key], dict):
+#                     if len(keys) == 1:
+#                         return configs[key]
+#                     else:
+#                         return Configuration._find_config(configs[key],
+#                                                           keys[1:])
+#
+#         return configs
+#
+#     @classmethod
+#     def get_configuration(cls, data_class, dict_):
+#         return from_dict(data_class,
+#                          cls._find_config(cls.configs, config_type.split(
+#                          '/')))
 
-    def __init__(self, config_file):
-        Configuration.config_file = config_file
-        try:
-            with open(self.config_file) as f:
-                Configuration.configs = yaml.full_load(f)
-        except FileNotFoundError:
-            print("Configuration file {} was not found.".format(self.config_file))
-
-    @staticmethod
-    def _find_config(configs, keys):
-        for key in configs.keys():
-            if key == keys[0]:
-                if isinstance(configs[key], dict):
-                    if len(keys) == 1:
-                        return configs[key]
-                    else:
-                        return Configuration._find_config(configs[key], keys[1:])
-
-        return configs
-
-    @classmethod
-    def get_configuration(cls, data_class, config_type):
-        return from_dict(data_class, cls._find_config(cls.configs, config_type.split('/')))
+def get_configuration(data_class, dict_):
+    return from_dict(data_class, dict_)
