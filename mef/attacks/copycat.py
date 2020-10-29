Corrected metric calculation

mef/attacks/copycat.py
@@ -7,13 +7,13 @@ from .base import Base
 
 class CopyCat(Base):
 
-    def __init__(self, victim_model, substitute_model, training_epochs=1000,
-                 early_stop_tolerance=10, evaluation_frequency=2,
-                 val_size=0.2, batch_size=64, save_loc="./cache/copycat",
-                 gpus=0, seed=None, deterministic=True, debug=False,
-                 precision=32):
-        optimizer = torch.optim.SGD(substitute_model.parameters(),
-                                    lr=0.01, momentum=0.8)
+    def __init__(self, victim_model, substitute_model, num_classes,
+                 training_epochs=1000, early_stop_tolerance=10,
+                 evaluation_frequency=2, val_size=0.2, batch_size=64,
+                 save_loc="./cache/copycat", gpus=0, seed=None,
+                 deterministic=True, debug=False, precision=32):
+        optimizer = torch.optim.SGD(substitute_model.parameters(), lr=0.01,
+                                    momentum=0.8)
         loss = F.cross_entropy
 
         super().__init__(victim_model, substitute_model, optimizer,
@@ -21,9 +21,9 @@ class CopyCat(Base):
                          early_stop_tolerance=early_stop_tolerance,
                          evaluation_frequency=evaluation_frequency,
                          val_size=val_size, batch_size=batch_size,
-                         save_loc=save_loc, gpus=gpus, seed=seed,
-                         deterministic=deterministic, debug=debug,
-                         precision=precision)
+                         num_classes=num_classes, save_loc=save_loc,
+                         gpus=gpus, seed=seed, deterministic=deterministic,
+                         debug=debug, precision=precision)
 
     def run(self, *args, **kwargs):
         self._parse_args(args, kwargs)

mef/utils/pytorch/lighting/module.py
@@ -1,16 +1,18 @@
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.metrics import functional as FM
 
 
 class MefModule(pl.LightningModule):
-    def __init__(self, model, optimizer=None, loss=None, lr_scheduler=None):
+    def __init__(self, model, num_classes, optimizer=None, loss=None,
+                 lr_scheduler=None):
         super().__init__()
         self._model = model
         self._optimizer = optimizer
         self._loss = loss
         self._lr_scheduler = lr_scheduler
         self.to(next(model.parameters()).device)
+        self._f1_macro = pl.metrics.Fbeta(num_classes, average="macro",
+                                          compute_on_step=False)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -22,26 +24,22 @@ class MefModule(pl.LightningModule):
 
     def _shared_step(self, batch):
         x, y = batch
-
         y_hat = self._model(x)
 
         if (y.numel() // len(y)) != 1:
             y = torch.argmax(y, dim=1)
 
-        f1 = FM.f1_score(y_hat, y, class_reduction="macro")
-
-        return f1
+        self._f1_macro(y_hat, y)
+        return
 
     def validation_step(self, batch, batch_idx):
-        f1 = self._shared_step(batch)
-        self.log_dict({"val_f1": f1}, prog_bar=True)
-
+        self._shared_step(batch)
+        self.log("val_f1", self._f1_macro, prog_bar=True, on_epoch=True)
         return
 
     def test_step(self, batch, batch_idx):
-        f1 = self._shared_step(batch)
-        self.log_dict({"test_f1": f1}, prog_bar=True)
-
+        self._shared_step(batch)
+        self.log("test_f1", self._f1_macro, on_epoch=True)
         return
 
     def configure_optimizers(self):