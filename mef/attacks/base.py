Corrected mistake in base.py and corrected calculation of accuracy and f1 score during validation when using binary output

mef/attacks/base.py
@@ -141,7 +141,7 @@ class Base(ABC):
 
         sub_test_labels = self._get_predictions(self._substitute_model,
                                                 self._test_set)
-        if len(vict_test_labels.size()) == 1:
+        if len(sub_test_labels.size()) == 1:
             sub_test_labels = torch.round(sub_test_labels).numpy()
         else:
             sub_test_labels = torch.argmax(sub_test_labels, dim=-1).numpy()

mef/utils/pytorch/lighting/module.py
@@ -91,7 +91,9 @@ class MefModel(pl.LightningModule):
         if self.device.type == "cuda":
             output = output.cuda()
 
-        if len(y.size()) > 1:
+        if len(y.size()) == 1:
+            y = torch.round(y)
+        else:
             y = torch.argmax(y, dim=-1)
 
         self._accuracy(output, y)