Corrected calculation of aggreement score

mef/attacks/base.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 from mef.utils.logger import set_up_logger
 from mef.utils.pytorch.datasets import CustomDataset, MefDataset
-from mef.utils.pytorch.functional import get_labels
+from mef.utils.pytorch.functional import get_labels, get_prob_dist
 from mef.utils.pytorch.lighting.module import TrainingModel, \
     VictimModel
 from mef.utils.pytorch.lighting.trainer import get_trainer_with_settings
@@ -139,7 +139,7 @@ class Base(ABC):
 
         sub_test_labels = self._get_predictions(self._substitute_model,
                                                 self._test_set)
-        sub_test_labels = get_labels(sub_test_labels).numpy()
+        sub_test_labels = get_labels(get_prob_dist(sub_test_labels)).numpy()
 
         agreement_count = np.sum(vict_test_labels == sub_test_labels)
         self._logger.info("Agreement count: {}".format(agreement_count))

mef/utils/pytorch/functional.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn.functional as F
 
 
 # Cross entropy for soft-labels
@@ -14,13 +13,13 @@ def soft_cross_entropy(logits, targets, weights=None):
 
 def get_prob_dist(logits):
     if len(logits.size()) == 1:
-        return F.sigmoid(logits)
+        return torch.sigmoid(logits)
     else:
-        return F.softmax(logits, dim=-1)
+        return torch.softmax(logits, dim=-1)
 
 
-def get_labels(logits):
-    if len(logits.size()) == 1:
-        return torch.round(logits)
+def get_labels(output):
+    if len(output.size()) == 1:
+        return torch.round(output)
     else:
-        return torch.argmax(logits, dim=-1)
+        return torch.argmax(output, dim=-1)