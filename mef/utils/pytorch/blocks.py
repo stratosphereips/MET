1f04edb6ad8cb4f9b04971e7ce5dc72173cb8663
Shanigen <vkaraf@gmail.com>
2021-01-05 15:36:17 +0100

    Removed unused methods

--- mef/utils/pytorch/blocks.py
+++ mef/utils/pytorch/blocks.py
@@ -1,51 +1,4 @@
 import torch.nn as nn
-from torch import optim
-
-
-def return_optimizer(model, optimizer_config):
-    """Prepares optimizer for the model.
-
-    Args:
-        model (Mnet/Nin): Instance of the training model
-        optimizer_config (OptimizerDetails): Configuration for the optimizer
-
-    Raises:
-        ValueError: Raised if the optimizer type is not one of
-        {SGD, ADAM, RMSprop}
-
-    Returns:
-        SGD/Adam/RMSprop: Instance of the optimizer
-    """
-
-    optimizer_name = optimizer_config.name
-    if optimizer_name == "SGD":
-        optimizer = optim.SGD(model.parameters(),
-                              lr=optimizer_config.lr,
-                              momentum=optimizer_config.momentum)
-    elif optimizer_name == "ADAM":
-        optimizer = optim.Adam(model.parameters(),
-                               lr=optimizer_config.lr)
-    elif optimizer_name == "RMSprop":
-        optimizer = optim.RMSprop(model.parameters(),
-                                  lr=optimizer_config.lr,
-                                  momentum=optimizer_config.momentum)
-    else:
-        raise ValueError(
-                "Optimizer name should be one of {SGD, ADAM, RMSprop}.")
-
-    return optimizer
-
-
-def return_loss_function(loss_function_name):
-    if loss_function_name == "cross_entropy":
-        loss_function = nn.CrossEntropyLoss()
-    elif loss_function_name == "mse":
-        loss_function = nn.MSELoss()
-    else:
-        raise ValueError(
-                "Loss function name for should be on of {cross_entropy, mse}")
-
-    return loss_function
 
 
 def return_act(activation, inplace=False):
@@ -62,27 +15,6 @@ def return_act(activation, inplace=False):
                 "Activation type should be one of {relu, elu, prelu, tanh}.")
 
     return act_func
-
-
-def return_drop(type, p=0.5, inplace=False):
-    """Returns instance of dropout type.
-
-    Args:
-        drop (str): Name of the dropout
-
-    Raises:
-        ValueError: Raised if the dropout name is not 'normal'
-
-    Returns:
-        dropout(): Instance of the dropout function.
-    """
-    if type == "normal":
-        drop_func = nn.Dropout(p=p, inplace=inplace)
-    elif type == "2d":
-        drop_func = nn.Dropout2d(p=p, inplace=inplace)
-    else:
-        raise ValueError("Dropout type must be one of {normal, 2d}.")
-    return drop_func
 
 
 def return_batch_norm(type, input_channels, eps, momentum, affine=True):
@@ -117,46 +49,4 @@ class ConvBlock(nn.Module):
         if self._use_batch_norm:
             x = self.batch_norm(x)
         x = self.act(x)
-        return x
-
-
-class LinearBlock(nn.Module):
-    def __init__(self, in_features, out_features, bias=True,
-                 act="relu", dropout="none"):
-        super().__init__()
-        self._conv = nn.Linear(in_features, out_features, bias=bias)
-        self._act = return_act(act, inplace=True)
-        self._dropout = nn.Dropout(inplace=True)
-        self._dropout_option = dropout
-
-    def forward(self, x):
-        x = self._conv(x)
-        x = self._act(x)
-        if self._dropout_option == 'normal':
-            x = self._dropout(x)
-        return x
-
-
-class MlpBlock(nn.Module):
-    def __init__(self, input_dim, output_dim, n_layer=2, hidden_dim=10,
-                 activation="relu"):
-        super().__init__()
-        self._n_layer = n_layer
-        self._hidden_dim = hidden_dim
-        self._input_dim = input_dim
-        self._output_dim = output_dim
-
-        self._act = return_act(activation, inplace=True)
-
-        self._fc1 = nn.Linear(self._input_dim, self._hidden_dim)
-        fc_iter = []
-        for _ in range(self._n_layer - 2):
-            fc_iter.append(LinearBlock(self._hidden_dim, self._hidden_dim))
-        self._fc_iter = nn.Sequential(*fc_iter)
-        self._fc_final = nn.Linear(self._hidden_dim, self._output_dim)
-
-    def forward(self, x):
-        x = self._act(self._fc1(x))
-        x = self._fc_iter(x)
-        x = self._fc_final(x)
         return x
