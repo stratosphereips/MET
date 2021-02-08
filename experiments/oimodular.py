b5355934bcc74d4819830d70f2980b2e489fead7
Shanigen <vkaraf@gmail.com>
2021-02-08 14:45:31 +0100

    Refactored code

--- experiments/oimodular.py
+++ experiments/oimodular.py
@@ -12,6 +12,7 @@ from mef.utils.pytorch.datasets import split_dataset
 from mef.utils.pytorch.datasets.vision.oimodular import OIModular
 from mef.utils.pytorch.models.vision import SimpleNet, ResNet
 
+
 def getr_args():
     parser = ArgumentParser(description="OImodular experiment")
     parser.add_argument("--simplenet", action="store_true",
@@ -20,15 +21,17 @@ def getr_args():
                         description="Resolution of samples")
     parser.add_argument("--num_classes", default=5, type=int,
                         description="Which number of classes to use. Can"
-                        "be one of {5, 17, 51} (Default: 5)")
+                                    "be one of {5, 17, 51} (Default: 5)")
     parser.add_argument("--batch_size", default=64, type=int,
                         description="Batch size which should be used"
-                        " (Default: 64)")
+                                    " (Default: 64)")
     parser.add_argument("--oi-dir", default="./cache/data", type=str,
                         description="Location where OpenImagesModular dataset"
-                        " is or should be located (Default: ./cache/data)")
+                                    " is or should be located (Default: "
+                                    "./cache/data)")
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = getr_args()
@@ -38,20 +41,20 @@ if __name__ == "__main__":
     train_set = OIModular("/data/vit/openimagesv6", args.num_classes,
                           download=True, transform=transform)
     test_set = OIModular("/data/vit/openimagesv6", args.num_classes,
-                         train=False, download=True,
-                         transform=transform)
+                         train=False, download=True, transform=transform)
 
     train_set, val_set = split_dataset(train_set, 0.2)
 
     if args.simplenet:
-        test_model = SimpleNet((3, 224, 224), args.num_classes)
+        test_model = SimpleNet((3, args.resolution, args.resolution),
+                               args.num_classes)
     else:
         test_model = ResNet("resnet_34", args.num_classes)
-    
+
     optimizer = torch.optim.Adam(test_model.parameters())
     loss = torch.nn.functional.cross_entropy
 
     train_victim_model(test_model, optimizer, loss, train_set,
-                       args.num_classes, 1000, arg.batch_size, 16,
+                       args.num_classes, 1000, args.batch_size, 16,
                        val_set, test_set, gpus=1,
                        save_loc=f"./cache/OIModular{args.num_classes}/")
