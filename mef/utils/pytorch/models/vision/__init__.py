5c7877238269b5f5133558936c75c69e4ef4eebe
Shanigen <vkaraf@gmail.com>
2021-02-09 13:03:53 +0100

    Add simpnet and refactored code

--- mef/utils/pytorch/models/vision/__init__.py
+++ mef/utils/pytorch/models/vision/__init__.py
@@ -3,4 +3,5 @@ from .at_cnn import AtCnn
 from .half_alexnet import HalfAlexNet, HalfAlexNetSmall
 from .resnet import ResNet
 from .simplenet import SimpleNet
+from .simpnet import SimpNet
 from .vgg import Vgg
