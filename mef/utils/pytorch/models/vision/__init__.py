837c3efcc287ebf8f5d3aa1e292f2dd291c5c20f
Shanigen <vkaraf@gmail.com>
2021-03-29 01:23:27 +0200

    Add LeNet and HalfLeNet

--- mef/utils/pytorch/models/vision/__init__.py
+++ mef/utils/pytorch/models/vision/__init__.py
@@ -1,6 +1,8 @@
 from .alexnet import AlexNet, AlexNetSmall
 from .GenericCNN import GenericCNN
 from .half_alexnet import HalfAlexNet, HalfAlexNetSmall
+from .half_lenet import HalfLeNet
+from .lenet import LeNet
 from .resnet import ResNet
 from .simplenet import SimpleNet
 from .simpnet import SimpNet
---
+++ mef/utils/pytorch/models/vision/half_lenet.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+
+from mef.utils.pytorch.models.vision.base import Base
+
+
+class HalfLeNet(Base):
+    def __init__(
+        self, num_classes, return_hidden=False,
+    ):
+        super().__init__(num_classes)
+        self._return_hidden = return_hidden
+
+        self._convs = [
+            nn.Conv2d(in_channels=1, out_channels=3, kernel_size=5, stride=1,),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=5, stride=1,),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+        ]
+
+        self._convs = nn.Sequential(*self._convs)
+
+        def init_weights(m):
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(
+                    m.weight.data, gain=nn.init.calculate_gain("relu")
+                )
+
+        self._convs.apply(init_weights)
+
+        self._fcs = [
+            nn.Linear(8 * 5 * 5, 120),
+            nn.ReLU(inplace=True),
+            nn.Linear(120, 84),
+            nn.ReLU(inplace=True),
+            nn.Linear(84, num_classes),
+        ]
+
+        self._fcs = nn.Sequential(*self._fcs)
+
+    def forward(self, x):
+        hidden = self._convs(x)
+        hidden = hidden.view(-1, 8 * 5 * 5)
+        logits = self._fcs(hidden)
+
+        if self._return_hidden:
+            return logits, hidden
+
+        return logits
