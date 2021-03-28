624908975c580052344abdc06dd57f389eaaf3f6
Shanigen <vkaraf@gmail.com>
2021-03-29 01:23:02 +0200

    Added missing import of FashionMnist

--- mef/utils/pytorch/datasets/vision/__init__.py
+++ mef/utils/pytorch/datasets/vision/__init__.py
@@ -1,6 +1,6 @@
 from .caltech256 import Caltech256
 from .cifar import Cifar10, Cifar100
 from .imagenet1000 import ImageNet1000
-from .mnist import Mnist
+from .mnist import Mnist, FashionMnist
 from .stl import Stl10
 from .indoor67 import Indoor67
