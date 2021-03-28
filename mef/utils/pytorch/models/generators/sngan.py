a727995a9d804303dc3f45c1df146d2d17e9c52a
Shanigen <vkaraf@gmail.com>
2021-03-28 12:55:24 +0200

    Add support for transformations in sngan

--- mef/utils/pytorch/models/generators/sngan.py
+++ mef/utils/pytorch/models/generators/sngan.py
@@ -1,3 +1,5 @@
+from typing import Callable, Optional
+
 import torch.nn as nn
 
 from torch_mimicry.nets import sngan
@@ -11,7 +13,12 @@ sngan_types = {
 
 
 class Sngan(nn.Module):
-    def __init__(self, checkpoint_file: str, resolution: int, gpu: bool = False):
+    def __init__(
+        self,
+        checkpoint_file: str,
+        resolution: int,
+        transform: Optional[Callable] = None,
+    ):
         super().__init__()
 
         if resolution not in [32, 48, 64, 128]:
@@ -20,9 +27,16 @@ class Sngan(nn.Module):
         self._generator = sngan_types[resolution]()
         self._latent_dim = 128
         self._generator.restore_checkpoint(checkpoint_file)
+        self._transform = transform
 
-        if gpu:
-            self._generator.cuda()
+    def cuda(self):
+        self.to("cuda")
+        self._generator.cuda()
 
     def forward(self, z):
-        return self._generator(z)
+        x = self._generator(z)
+
+        if self._transform is not None:
+            x = self._transform(x)
+
+        return x
